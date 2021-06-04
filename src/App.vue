39675fdc0e08dbbc697b9734b3cdc32819a17acf
CN-3211 <372083746@qq.com>
2021-06-04 09:18:35 +0800

    布局修改，地图满屏显示；后续需要补充通用样式，尤其是box-sizing

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,17 @@
 <!--
  * @Date: 2021-06-02 17:21:44
  * @LastEditors: huangzh873
- * @LastEditTime: 2021-06-02 18:03:24
+ * @LastEditTime: 2021-06-04 09:11:01
  * @FilePath: \cesium-web-vue\src\App.vue
 -->
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="layout">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
-  <router-view/>
 </template>
 
 <style lang="scss">
@@ -19,11 +21,21 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  height: 100%;
+  width: 100%;
+}
+#layout {
+  height: 100%;
+  padding-top: 80px;
+  box-sizing: border-box;
 }
 
 #nav {
   padding: 30px;
-
+  height: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
   a {
     font-weight: bold;
     color: #2c3e50;
