3ba9553c785a828bb586cfacb382151b7fbf5e2b
CN-3211 <372083746@qq.com>
2021-06-16 09:47:41 +0800

    element-plus插件导入

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
 /*
  * @Date: 2021-06-02 17:21:44
  * @LastEditors: huangzh873
- * @LastEditTime: 2021-06-03 11:17:03
+ * @LastEditTime: 2021-06-10 09:16:08
  * @FilePath: \cesium-web-vue\src\main.ts
  */
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import "cesium/Build/Cesium/Widgets/widgets.css";
+import "cesium/Build/Cesium/Widgets/widgets.css"
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 
-createApp(App).use(store).use(router).mount('#app')
+
+createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
