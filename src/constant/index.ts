e8768e90438645ea69963f9d7656708699a3c7fa
CN-3211 <372083746@qq.com>
2021-06-17 16:50:02 +0800

    一些常用的常量统一放在constant文件夹下使用

---
+++ src/constant/index.ts
@@ -0,0 +1,16 @@
+/*
+ * @Date: 2021-06-17 16:01:31
+ * @LastEditors: huangzh873
+ * @LastEditTime: 2021-06-17 16:11:52
+ * @FilePath: \cesium-web-vue\src\constant\index.ts
+ */
+export const enum direction {
+  LEFT = "Left",
+  RIGHT = "Right",
+  TOP = "Top",
+  BOTTOM = "Bottom",
+  EAST = "East",
+  WEST = "West",
+  SOUTH = "South",
+  NORTH = "North"
+}
