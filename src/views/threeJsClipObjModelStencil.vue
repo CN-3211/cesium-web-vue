f69151932b7205f954b69a8696e2f797d0097688
CN-3211 <372083746@qq.com>
2021-07-09 21:02:03 +0800

    模型剖切，别问我是怎么做到的，反正就成了！

--- src/views/threeJsClipObjModelStencil.vue
+++ src/views/threeJsClipObjModelStencil.vue
@@ -1,7 +1,7 @@
 <!--
  * @Date: 2021-06-30 19:52:31
  * @LastEditors: huangzh873
- * @LastEditTime: 2021-07-05 09:16:25
+ * @LastEditTime: 2021-07-09 21:01:17
  * @FilePath: \cesium-web-vue\src\views\threeJsClipObjModelStencil.vue
 -->
 <template>
@@ -14,7 +14,6 @@
 import { defineComponent, onMounted } from 'vue';
 
 import * as THREE from 'three';
-import { DDSLoader } from 'three/examples/jsm/loaders/DDSLoader.js';
 import { MTLLoader } from 'three/examples/jsm/loaders/MTLLoader.js';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
@@ -26,26 +25,20 @@ let scene:THREE.Scene
 let camera:THREE.PerspectiveCamera
 let planes:THREE.Plane[] = [
   new THREE.Plane( new THREE.Vector3( - 1, 0, 0 ), 0 ),
-  // new THREE.Plane( new THREE.Vector3( 0, - 1, 0 ), 0 ),
+  new THREE.Plane( new THREE.Vector3( 0, - 1, 0 ), 0 ),
   // new THREE.Plane( new THREE.Vector3( 0, 0, - 1 ), 0 )
 ];
 
-
-/**
- * 内容: 生成的模型切面(可见)
- * 作用: animation中,防止视角切换切面不显示
- */
-let planeObjects:THREE.Mesh[] = []
 export default defineComponent({
   setup() {
-    onMounted(() => {
-      init()
+    onMounted(async () => {
+      await init()
       animate()
     })
   }
 })
 
-function init():void {
+async function init():Promise<void> {
   const container:HTMLElement|null = document.getElementById("threeJsClipObjModelStencil");
   if(!container) throw new Error("没有获取到container");
   
@@ -86,6 +79,7 @@ function init():void {
       console.log( Math.round( percentComplete ) + '% downloaded' );
     }
   };
+
   new MTLLoader().setPath('obj/地层切割模型/').load('mdl1.mtl', (materials) => {
     materials.preload();
     new OBJLoader().setMaterials(materials).setPath('obj/地层切割模型/').load("mdl1.obj", (layerModel:THREE.Group) => {
@@ -104,65 +98,42 @@ function init():void {
           opacity: 0.1
         })
       );
-      console.log('boundingBox.max.y :>> ', boundingBox.max.y);
-      console.log('boundingBox.min.y :>> ', boundingBox.min.y);
       scene.add(mesh)
 
 
       const x= (boundingBox.max.x+boundingBox.min.x)/2;
       const y= (boundingBox.max.y+boundingBox.min.y)/2;
       const z= (boundingBox.max.z+boundingBox.min.z)/2;
-      console.log('boundingBox.max.z :>> ', boundingBox.max.z);
-      console.log('boundingBox.min.z :>> ', boundingBox.min.z);
       layerModel.position.set(-x, -y, -z);
+      const group:THREE.Group = new THREE.Group();
       
       layerModel.traverse((child:THREE.Object3D) => {
         if (child instanceof THREE.Mesh) {
           child.material.clippingPlanes = planes
-          child.castShadow = true; // ??
-          child.renderOrder = 6; // ??
+          const geometry = child.geometry.clone();
+          const meshMaterial = child.material as any;
+          const baseMat:THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
+            color: meshMaterial.color
+          });
+          const mat0 = baseMat.clone();
+          mat0.side = THREE.BackSide;
+          mat0.clippingPlanes = planes
+          const mesh0:THREE.Mesh = new THREE.Mesh( geometry, mat0 );
+
+          const mat1 = baseMat.clone();
+          mat1.side = THREE.FrontSide;
+          mat1.clippingPlanes = planes;
+
+          const mesh1 = new THREE.Mesh( geometry, mat1 );
+      
+          group.add( mesh0 );
+          group.add( mesh1 );
         }
       })
-      objectGroup.add(layerModel);
+      group.scale.set(0.001,0.001,0.001);
+      group.position.set(-x, -y, -z);
 
-      const planeGeom = new THREE.PlaneBufferGeometry( 1, 1 );
-      
-
-      for(let i = 0; i < planes.length; i++) {
-        const poGroup:THREE.Group = new THREE.Group();
-        const plane = planes[i]
-
-        layerModel.children.forEach(itemModel => {
-          const _itemModel = itemModel as THREE.Mesh;
-          const geometry = _itemModel.geometry.clone();
-          const meshMaterial = _itemModel.material as any;
-          geometry.translate(-x/0.001, -y/0.001, -z/0.001);
-
-          const stencilGroup = createPlaneStencilGroup( geometry, plane, i + 1, meshMaterial.color );
-          stencilGroup.scale.set(0.001,0.001,0.001)
-          objectGroup.add(stencilGroup)
-        })
-
-        // plane会被其他clipping planes裁剪
-        const planeMat = new THREE.MeshStandardMaterial({
-          color: 0xffff00,
-          clippingPlanes: planes.filter( p => p !== plane ),
-
-          stencilWrite: true,
-          stencilRef: 0,
-          stencilFunc: THREE.NotEqualStencilFunc,
-          stencilFail: THREE.ReplaceStencilOp,
-          stencilZFail: THREE.ReplaceStencilOp,
-          stencilZPass: THREE.ReplaceStencilOp,
-        })
-
-        const po = new THREE.Mesh(planeGeom, planeMat);
-        po.onAfterRender = renderer => { renderer.clearStencil() }
-        po.renderOrder = i + 1.2;
-        poGroup.add(po);
-        planeObjects.push(po);
-        scene.add(poGroup)
-      }
+      objectGroup.add(group);
 
       // UI添加
       const params = {
@@ -230,78 +201,13 @@ function init():void {
   const controls:OrbitControls = new OrbitControls(camera, renderer.domElement);
   controls.minDistance = 1.5;
   controls.maxDistance = 2.5;
-
-  
-
 }
 
 function animate():void {
   requestAnimationFrame(animate);
-  for ( let i = 0; i < planeObjects.length; i ++ ) {
-    let plane = planes[ i ];
-    let po = planeObjects[ i ];
-
-    plane.coplanarPoint( po.position );
-    
-    // mesh.lookAt起什么作用
-    po.lookAt(
-        po.position.x - plane.normal.x,
-        po.position.y - plane.normal.y,
-        po.position.z - plane.normal.z,
-    );
-  }
   renderer.render( scene, camera );
 }
 
-/**
- * @description: 初始化地质模型的每一个格网的模板缓冲，分为正面和背面。背面模板缓冲的值设置为1，正面模板缓冲的值设置为-1
- * @param {*} layerModel
- * @param {*} plane
- * @param {*} renderOrder
- * @param {*} meshColor
- * @return {*}
- */
-function createPlaneStencilGroup(layerModel:THREE.BufferGeometry, plane:THREE.Plane, renderOrder:number, meshColor:THREE.Color):THREE.Group {
-  const group:THREE.Group = new THREE.Group();
-  const baseMat:THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
-    color: meshColor
-  });
-  baseMat.depthWrite = false;
-  baseMat.depthTest = false;
-  baseMat.colorWrite = false;
-  baseMat.stencilWrite = true;
-  baseMat.stencilFunc = THREE.AlwaysStencilFunc;
-
-  // 
-  const mat0 = baseMat.clone();
-  mat0.side = THREE.BackSide;
-  mat0.clippingPlanes = [ plane ];
-  // IncrementWrapStencilOp将当前stencil value增加1
-  mat0.stencilFunc = THREE.AlwaysStencilFunc;
-  // mat0.stencilFail = THREE.IncrementWrapStencilOp;
-  mat0.stencilZFail = THREE.IncrementWrapStencilOp;
-  mat0.stencilZPass = THREE.IncrementWrapStencilOp;
-
-  const mesh0:THREE.Mesh = new THREE.Mesh( layerModel, mat0 );
-  mesh0.renderOrder = renderOrder;
-  group.add( mesh0 );
-
-  // front faces
-  const mat1 = baseMat.clone();
-  mat1.side = THREE.FrontSide;
-  mat1.clippingPlanes = [ plane ];
-  // DecrementWrapStencilOp将当前stencil value减少1
-  // mat1.stencilFail = THREE.DecrementWrapStencilOp;
-  mat1.stencilZFail = THREE.DecrementWrapStencilOp;
-  mat1.stencilZPass = THREE.DecrementWrapStencilOp;
-
-  const mesh1 = new THREE.Mesh( layerModel, mat1 );
-  mesh1.renderOrder = renderOrder;
-
-  group.add( mesh1 );
-
-  return group
-}
 </script>
 
 <style>
