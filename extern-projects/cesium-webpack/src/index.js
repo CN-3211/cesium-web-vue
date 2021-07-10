9adc0ae1bce44e29076304d46ba4721cb6d77fec
CN-3211 <372083746@qq.com>
2021-07-10 11:01:34 +0800

    webpack---杂项更改（请忽略）

--- extern-projects/cesium-webpack/src/index.js
+++ extern-projects/cesium-webpack/src/index.js
@@ -1,8 +1,8 @@
 /*
  * @Date: 2021-06-03 11:33:56
  * @LastEditors: huangzh873
- * @LastEditTime: 2021-06-03 16:39:58
- * @FilePath: \cesium-webpack\src\index.js
+ * @LastEditTime: 2021-07-10 10:31:12
+ * @FilePath: \cesium-web-vue\extern-projects\cesium-webpack\src\index.js
  */
 // import xxx form 'cesium';这里的cesium指的是node_modules文件夹下的index.js文件（这里指index.cjs文件），该文件export了cesium.js文件
 // 因此我们可以通过import * as Cesium from "cesium"的方式，把cesium.js文件下的所有class都挂在Cesium变量下进行使用
@@ -11,11 +11,9 @@
 
 // import * as Cesium from 'cesium'; √
 // import Cesium from "cesium"; ×
-import { Viewer } from "cesium"
 
 // 这里的$cesium是在webpack.config.js中定义的别名，路径为"node_modules/cesium/Source"
 // node_modules文件夹下的cesium文件夹最外层的index.cjs文件中，没有export相关的css文件，不能够通过import "cesium/Source/Widgets/widgets";直接加载css
 // 因此需要通过设置别名$cesium访问（暂时没想到更好的解决方法）
 import "$cesium/Widgets/widgets.css"
-var viewer = new Viewer('cesiumContainer');
-console.log('Hello World!111222');
+import "./initViewer.js"
---
+++ extern-projects/cesium-webpack/src/initViewer.js
@@ -0,0 +1,9 @@
+/*
+ * @Date: 2021-07-10 10:28:00
+ * @LastEditors: huangzh873
+ * @LastEditTime: 2021-07-10 11:00:13
+ * @FilePath: \cesium-web-vue\extern-projects\cesium-webpack\src\initViewer.js
+ */
+import { Viewer } from 'cesium';
+const viewer = new Viewer('cesiumContainer');
+// export default viewer;
