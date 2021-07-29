52a03db969ed6c10888642e764202ba0ab5461e7
CN-3211 <372083746@qq.com>
2021-07-29 16:34:34 +0800

    简单修改了一下，基于ts重写的webGL函数库，并理解initShader每一步的作用

--- src/utils/webGL-base.ts
+++ src/utils/webGL-base.ts
@@ -1,8 +1,8 @@
 /*
  * @Date: 2021-06-23 19:53:02
  * @LastEditors: huangzh873
- * @LastEditTime: 2021-06-24 09:54:20
- * @FilePath: \cesium-web-vue\src\utils\webGL.ts
+ * @LastEditTime: 2021-07-29 16:19:35
+ * @FilePath: \cesium-web-vue\src\utils\webGL-base.ts
  */
 
 function ready (callback:() => void):void {
@@ -15,13 +15,11 @@ function ready (callback:() => void):void {
  * @param fshader a fragment shader program (string)
  * @return true, if the program object was created and successfully made current 
  */
-function initShaders(gl:WebGLRenderingContext, vshader:string, fshader:string):WebGLProgram|null {
+function initShaders(gl:WebGLRenderingContext, vshader:string, fshader:string):WebGLProgram {
   const program = createProgram(gl, vshader, fshader);
   if (!program) {
-    console.log('Failed to create program');
-    return null;
+    throw new Error("Failed to create program");
   }
-
   gl.useProgram(program);
 
   return program;
@@ -34,18 +32,15 @@ function initShaders(gl:WebGLRenderingContext, vshader:string, fshader:string):W
  * @param fshader a fragment shader program (string)
  * @return created program object, or null if the creation has failed
  */
- function createProgram(gl:WebGLRenderingContext, vshader:string, fshader:string):WebGLProgram|null {
+ function createProgram(gl:WebGLRenderingContext, vshader:string, fshader:string):WebGLProgram {
   // Create shader object
   const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vshader);
   const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fshader);
-  if (!vertexShader || !fragmentShader) {
-    return null;
-  }
 
   // Create a program object
   const program = gl.createProgram();
   if (!program) {
-    return null;
+    throw new Error("created program failed");
   }
 
   // Attach the shader objects
@@ -59,11 +54,11 @@ function initShaders(gl:WebGLRenderingContext, vshader:string, fshader:string):W
   const linked = gl.getProgramParameter(program, gl.LINK_STATUS);
   if (!linked) {
     const error = gl.getProgramInfoLog(program);
-    console.log('Failed to link program: ' + error);
     gl.deleteProgram(program);
     gl.deleteShader(fragmentShader);
     gl.deleteShader(vertexShader);
-    return null;
+    throw new Error("Failed to link program: " + error);
+    
   }
   return program;
 }
@@ -75,12 +70,11 @@ function initShaders(gl:WebGLRenderingContext, vshader:string, fshader:string):W
  * @param source shader program (string)
  * @return created shader object, or null if the creation has failed.
  */
- function loadShader(gl:WebGLRenderingContext, type:number, source:string) {
+ function loadShader(gl:WebGLRenderingContext, type:number, source:string): WebGLShader {
   // Create shader object
   const shader = gl.createShader(type);
   if (shader == null) {
-    console.log('unable to create shader');
-    return null;
+    throw new Error("unable to create shader");
   }
 
   // Set the shader program
@@ -93,9 +87,8 @@ function initShaders(gl:WebGLRenderingContext, vshader:string, fshader:string):W
   const compiled = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
   if (!compiled) {
     const error = gl.getShaderInfoLog(shader);
-    console.log('Failed to compile shader: ' + error);
     gl.deleteShader(shader);
-    return null;
+    throw new Error("Failed to compile shader:" + error);
   }
 
   return shader;
