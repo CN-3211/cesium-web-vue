1f899d4a76b5af47e4741a124d8971bac10a6f26
CN-3211 <372083746@qq.com>
2021-08-04 11:25:12 +0800

    三面模板缓冲切割---代码未优化，未解耦

--- src/views/threeJsClipObjModelStencil.vue
+++ src/views/threeJsClipObjModelStencil.vue
@@ -1,52 +1,70 @@
 <!--
  * @Date: 2021-06-30 19:52:31
  * @LastEditors: huangzh873
- * @LastEditTime: 2021-08-02 10:04:30
+ * @LastEditTime: 2021-08-04 11:22:46
  * @FilePath: \cesium-web-vue\src\views\threeJsClipObjModelStencil.vue
 -->
 <template>
   <div class="threeJsClipObjModelStencil" id="threeJsClipObjModelStencil">
-    <div class="block">
-      <span class="demonstration">平面距离</span>
-      <el-slider v-model="distance" :min="-1" :max="1" :step="0.001" @input="onSlide"></el-slider>
+    <div class="controlGroup">
+      <div class="block">
+        <span class="demonstration">X平面距离</span>
+        <el-slider v-model="distance.x" :min="-1" :max="1" :step="0.001" @input="onSlideX"></el-slider>
+      </div>
+      <div class="block">
+        <span class="demonstration">Y平面距离</span>
+        <el-slider v-model="distance.y" :min="-1" :max="1" :step="0.001" @input="onSlideY"></el-slider>
+      </div>
+      <div class="block">
+        <span class="demonstration">Z平面距离</span>
+        <el-slider v-model="distance.z" :min="-1" :max="1" :step="0.001" @input="onSlideZ"></el-slider>
+      </div>
     </div>
   </div>
 </template>
-
 <script lang="ts">
-import { defineComponent, onMounted, ref } from "vue";
+// #region
+import { defineComponent, onMounted, reactive } from "vue";
 
 import * as THREE from "three";
 import { MTLLoader } from "three/examples/jsm/loaders/MTLLoader.js";
 import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { GUI } from "three/examples/jsm/libs/dat.gui.module.js";
-const distance = ref(0);
+const distance = reactive({
+  x: 0,
+  y: 0,
+  z: 0
+});
 
 let renderer: THREE.WebGLRenderer;
 let scene: THREE.Scene;
 let camera: THREE.PerspectiveCamera;
-let planes: THREE.Plane[] = [
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.value),
-  // new THREE.Plane(new THREE.Vector3(0, -1, 0), 0),
-  // new THREE.Plane( new THREE.Vector3( 0, 0, - 1 ), 0 )
+const modelNameArr = [
+  "1-2",
+  "3-1",
+  "3-2",
+  "3-4",
+  "4-1",
+  "4-4",
+  "8-1",
+  "8-4",
+  "11-1",
+  "11-4",
+  "17-1",
+  "17-2",
+  "17-3",
+  "17-4",
+  "17-5",
+  "g孤石",
 ];
-const planeObjects: THREE.Mesh[] = [];
+
+let planes: THREE.Plane[][] = [[], [], []];
+for (let i = 0; i < modelNameArr.length; i++) {
+  planes[0].push(new THREE.Plane(new THREE.Vector3(-1, 0, 0), distance.x));
+  planes[1].push(new THREE.Plane(new THREE.Vector3(0, -1, 0), distance.y));
+  planes[2].push(new THREE.Plane(new THREE.Vector3(0, 0, -1), distance.y));
+}
+const planeObjects: THREE.Mesh[][] = [ [], [], [] ];
 
 export default defineComponent({
   setup() {
@@ -54,12 +72,22 @@ export default defineComponent({
       await init();
       animate();
     });
-    const onSlide = () => {
-      planes.forEach(item => {
-        item.constant = distance.value;
+    const onSlideX = () => {
+      planes[0].forEach(item => {
+        item.constant = distance.x;
       })
     }
-    return { distance, onSlide }
+    const onSlideY = () => {
+      planes[1].forEach(item => {
+        item.constant = distance.y;
+      })
+    }
+    const onSlideZ = () => {
+      planes[2].forEach(item => {
+        item.constant = distance.z;
+      })
+    }
+    return { distance, onSlideX, onSlideY, onSlideZ }
   },
 });
 
@@ -96,7 +124,8 @@ async function init(): Promise<void> {
   const axisHelper = new THREE.AxesHelper(250);
   scene.add(axisHelper);
 
-  new handModels(scene);
+  const handModel = new handModels(scene);
+  await handModel.loadModels(scene)
 
   // render配置
   renderer = new THREE.WebGLRenderer();
@@ -117,22 +146,26 @@ async function init(): Promise<void> {
   );
   controls.minDistance = 1.5;
 }
+// #endregion
 
 function animate(): void {
   requestAnimationFrame(animate);
-  for (let i = 0; i < planeObjects.length; i++) {
-    let plane = planes[i];
-    let po = planeObjects[i];
+  for (let x = 0; x < planes.length; x++) {
+    for (let i = 0; i < modelNameArr.length; i++) {
+      let plane = planes[x][i];
+      let po = planeObjects[x][i];
 
-    plane.coplanarPoint(po.position);
+      plane.coplanarPoint(po.position);
 
-    // mesh.lookAt起什么作用
-    po.lookAt(
-      po.position.x - plane.normal.x,
-      po.position.y - plane.normal.y,
-      po.position.z - plane.normal.z
-    );
+      // mesh.lookAt起什么作用
+      po.lookAt(
+        po.position.x - plane.normal.x,
+        po.position.y - plane.normal.y,
+        po.position.z - plane.normal.z
+      );
+    }
   }
+  
   renderer.render(scene, camera);
 }
 
@@ -140,27 +173,9 @@ class handModels {
   objectGroup = new THREE.Group();
   constructor(scene: THREE.Scene) {
     scene.add(this.objectGroup);
-    this.loadModels(scene);
   }
   async loadModels(scene: THREE.Scene) {
-    const modelNameArr = [
-      "1-2",
-      "3-1",
-      "3-2",
-      "3-4",
-      "4-1",
-      "4-4",
-      "8-1",
-      "8-4",
-      "11-1",
-      "11-4",
-      "17-1",
-      "17-2",
-      "17-3",
-      "17-4",
-      "17-5",
-      "g孤石",
-    ];
+    
     const objAndMtls: any[] = [];
 
     for (let i = 0; i < modelNameArr.length; i++) {
@@ -178,70 +193,76 @@ class handModels {
         mtl,
       });
     }
-    for (let i = 0; i < objAndMtls.length; i++) {
-      const layerModel = objAndMtls[i].obj;
-      const layerMaterial: MTLLoader.MaterialCreator = objAndMtls[i].mtl;
-      layerModel.traverse((child: THREE.Object3D) => {
-        if (child instanceof THREE.Mesh) {
-          child.material.clippingPlanes = planes;
-          child.castShadow = true; // ??
-          child.renderOrder = 6; // ??
-        }
-      });
-      this.objectGroup.add(layerModel);
 
-      const planeGeom = new THREE.PlaneBufferGeometry(1, 1);
-      const poGroup: THREE.Group = new THREE.Group();
-      const plane = planes[i];
+    let clippingPlanes:THREE.Plane[] = []
+    planes.forEach(item => {
+      clippingPlanes = clippingPlanes.concat(item);
+    })
+    for (let x = 0; x < planes.length; x++) {
+      for (let i = 0; i < modelNameArr.length; i++) {
+        const layerModel = objAndMtls[i].obj;
+        layerModel.traverse((child: THREE.Object3D) => {
+          if (child instanceof THREE.Mesh) {
+            child.material.clippingPlanes = clippingPlanes;
+            child.castShadow = true; // ??
+            child.renderOrder = 6; // ??
+          }
+        });
+        this.objectGroup.add(layerModel);
 
-      layerModel.children.forEach((itemModel) => {
-        const _itemModel = itemModel as THREE.Mesh;
-        const geometry = _itemModel.geometry.clone();
-        const meshMaterial = _itemModel.material as any;
+        const planeGeom = new THREE.PlaneBufferGeometry(1, 1);
+        const poGroup: THREE.Group = new THREE.Group();
+        const plane = planes[x][i];
 
-        const stencilGroup = this.createPlaneStencilGroup(
-          geometry,
-          plane,
-          i + 1,
-          meshMaterial.color
-        );
-        stencilGroup.scale.set(0.001, 0.001, 0.001);
-        this.objectGroup.add(stencilGroup);
-      });
+        layerModel.children.forEach((itemModel) => {
+          const _itemModel = itemModel as THREE.Mesh;
+          const geometry = _itemModel.geometry.clone();
 
-      // plane会被其他clipping planes裁剪
-      const planeMat = new THREE.MeshStandardMaterial({
-        color: layerModel.children[0].material.color,
-        // color: "#FFFF00",
-        // clippingPlanes: planes.filter((p) => p !== plane),
-        stencilWrite: true,
-        stencilRef: 0,
-        stencilFunc: THREE.NotEqualStencilFunc,
-        stencilFail: THREE.ReplaceStencilOp,
-        stencilZFail: THREE.ReplaceStencilOp,
-        stencilZPass: THREE.ReplaceStencilOp,
-      });
+          const stencilGroup = this.createPlaneStencilGroup(
+            geometry,
+            plane,
+            i + 2.2
+          );
+          stencilGroup.scale.set(0.001, 0.001, 0.001);
+          this.objectGroup.add(stencilGroup);
+        });
 
-      const po = new THREE.Mesh(planeGeom, planeMat);
-      po.onAfterRender = (renderer) => {
-        renderer.clearStencil();
-      };
-      po.renderOrder = i + 1.2;
-      poGroup.add(po);
-      planeObjects.push(po);
-      scene.add(poGroup);
+        let result: THREE.Plane[] = [];
+        planes.forEach((item, index) => {
+          if(index !== x) {
+            result = result.concat(item);
+          }
+        })
+        // plane会被其他clipping planes裁剪
+        const planeMat = new THREE.MeshStandardMaterial({
+          color: layerModel.children[0].material.color,
+          clippingPlanes: result,
+          stencilWrite: true,
+          stencilRef: 0,
+          stencilFunc: THREE.NotEqualStencilFunc,
+          stencilFail: THREE.ReplaceStencilOp,
+          stencilZFail: THREE.ReplaceStencilOp,
+          stencilZPass: THREE.ReplaceStencilOp,
+        });
+
+        const po = new THREE.Mesh(planeGeom, planeMat);
+        po.onAfterRender = (renderer) => {
+          renderer.clearStencil();
+        };
+        po.renderOrder = i + 1.2;
+        poGroup.add(po);
+        planeObjects[x].push(po);
+        scene.add(poGroup);
+      }
     }
   }
   createPlaneStencilGroup(
     layerModel: THREE.BufferGeometry,
     plane: THREE.Plane,
-    renderOrder: number,
-    meshColor: THREE.Color
+    renderOrder: number
   ): THREE.Group {
     const group: THREE.Group = new THREE.Group();
-    const baseMat: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial({
-      color: meshColor,
-    });
+    const baseMat: THREE.MeshBasicMaterial = new THREE.MeshBasicMaterial();
     baseMat.depthWrite = false;
     baseMat.depthTest = false;
     baseMat.colorWrite = false;
@@ -254,7 +275,7 @@ class handModels {
     mat0.clippingPlanes = [plane];
     // IncrementWrapStencilOp将当前stencil value增加1
     mat0.stencilFunc = THREE.AlwaysStencilFunc;
-    // mat0.stencilFail = THREE.IncrementWrapStencilOp;
+    mat0.stencilFail = THREE.IncrementWrapStencilOp;
     mat0.stencilZFail = THREE.IncrementWrapStencilOp;
     mat0.stencilZPass = THREE.IncrementWrapStencilOp;
 
@@ -267,7 +288,7 @@ class handModels {
     mat1.side = THREE.FrontSide;
     mat1.clippingPlanes = [plane];
     // DecrementWrapStencilOp将当前stencil value减少1
-    // mat1.stencilFail = THREE.DecrementWrapStencilOp;
+    mat1.stencilFail = THREE.DecrementWrapStencilOp;
     mat1.stencilZFail = THREE.DecrementWrapStencilOp;
     mat1.stencilZPass = THREE.DecrementWrapStencilOp;
 
@@ -281,9 +302,23 @@ class handModels {
 }
 </script>
 
-<style>
+<style lang="scss">
 .threeJsClipObjModelStencil {
   width: 100%;
   height: 100%;
+  position: relative;
+  .controlGroup {
+    box-sizing: border-box;
+    width: 300px;
+    padding: 10px;
+    border-radius: 3%;
+    position: absolute;
+    top: 30px;
+    left: 10px;
+    background-color: rgba($color: #FFFFFF, $alpha: 0.2);
+    .block {
+      color: #FFFFFF;
+    }
+  }
 }
 </style>
