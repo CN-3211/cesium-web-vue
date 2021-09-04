b7fae786e8a21d7e53c18d0a5fb22c672315d365
CN-3211 <johndoe@example.com>
2021-09-04 19:44:40 +0800

    webGL编程指南---平行光的漫反射

--- extern-projects/webGL-practice/lesson08_light/lightedCube.js
+++ extern-projects/webGL-practice/lesson08_light/lightedCube.js
@@ -1,21 +1,25 @@
 /*
- * @Date: 2021-07-27 19:12:55
+ * @Date: 2021-07-05 09:49:35
  * @LastEditors: huangzh873
- * @LastEditTime: 2021-07-29 16:47:01
+ * @LastEditTime: 2021-09-04 19:36:28
  * @FilePath: \cesium-web-vue\extern-projects\webGL-practice\lesson08_light\lightedCube.js
  */
+
+/* ------------------------------ 顶点着色器和片元着色器开始 --------------------------------*/
 const VSHADER_SOURCE = `
   attribute vec4 a_Position;
   attribute vec4 a_Color;
   attribute vec4 a_Normal;
   uniform mat4 u_MvpMatrix;
-  uniform mat4 u_LightColor;
-  uniform mat4 u_LightDirection;
+  uniform vec3 u_LightColor;
+  uniform vec3 u_LightDirection;
   varying vec4 v_Color;
   void main() {
     gl_Position = u_MvpMatrix * a_Position;
     vec3 normal = normalize(vec3(a_Normal));
-    v_Color = a_Color;
+    float nDotL = max(dot(u_LightDirection, normal), 0.0);
+    vec3 diffuse = u_LightColor * vec3(a_Color) * nDotL;
+    v_Color = vec4(diffuse, a_Color.a);
   }
 `
 
@@ -26,20 +30,29 @@ const FSHADER_SOURCE = `
     gl_FragColor = v_Color;
   }
 `
+/* ------------------------------ 顶点着色器和片元着色器结束 --------------------------------*/
+
 window.onload = () => {
   const canvas = document.getElementById('webgl');
   const gl = getWebGLContext(canvas);
+  if (!gl) {
+    console.log('Failed to get the rendering context for WebGL');
+    return;
+  }
   if (!initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE)) {
     console.log('Failed to intialize shaders.');
     return;
   }
-  
   const n = initVertexBuffer(gl);
   const u_MvpMatrix = gl.getUniformLocation(gl.program, 'u_MvpMatrix');
+  const u_LightColor = gl.getUniformLocation(gl.program, 'u_LightColor');
+  const u_LightDirection = gl.getUniformLocation(gl.program, 'u_LightDirection');
+
   const modelMatrix = new Matrix4();
   const viewMatrix = new Matrix4();
   const projMatrix = new Matrix4();
   const mvpMatrix = new Matrix4();
+
 
   modelMatrix.setTranslate(0, 0, 0);
   viewMatrix.setLookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
@@ -49,59 +62,96 @@ window.onload = () => {
   mvpMatrix.set(projMatrix).multiply(viewMatrix).multiply(modelMatrix)
 
   gl.uniformMatrix4fv(u_MvpMatrix, false, mvpMatrix.elements);
+  // 设置白色光线
+  gl.uniform3f(u_LightColor, 1.0, 1.0, 1.0);
 
+  // 设置光线方向
+  const lightDirection = new Vector3([0.5, 3.0, 4.0]);
+  lightDirection.normalize();
+  gl.uniform3fv(u_LightDirection, lightDirection.elements);
+
+  // 设置背景颜色
   gl.clearColor(0, 0, 0, 1);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  // 开启深度测试
+  gl.enable(gl.DEPTH_TEST);
+  // 清除颜色缓冲区（类似清除模板缓冲区）
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
   gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
 }
 
+/**
+ * @description: 设置顶点坐标和颜色，蓝色三角形在最前面
+ * @param {WebGLRenderingContext}
+ * @return {Number} [n] 顶点数
+ */
 function initVertexBuffer(gl) {
-  const verticesColors = new Float32Array([
+  const vertices = new Float32Array([
     // 顶点坐标和颜色
-     1.0,  1.0,  1.0,     1.0,  0.0,  1.0,  // v0 White
-    -1.0,  1.0,  1.0,     1.0,  0.0,  1.0,  // v1 Magenta
-    -1.0, -1.0,  1.0,     1.0,  0.0,  1.0,  // v2 Red
-     1.0, -1.0,  1.0,     1.0,  0.0,  1.0,  // v3 Yellow
-     1.0, -1.0, -1.0,     1.0,  0.0,  1.0,  // v4 Green
-     1.0,  1.0, -1.0,     1.0,  0.0,  1.0,  // v5 Cyan
-    -1.0,  1.0, -1.0,     1.0,  0.0,  1.0,  // v6 Blue
-    -1.0, -1.0, -1.0,     1.0,  0.0,  1.0   // v7 Black
+    1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,  // v0-v1-v2-v3 front
+    1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,  // v0-v3-v4-v5 right
+    1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
+   -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
+   -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
+    1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0   // v4-v7-v6-v5 back
   ])
+  const colors = new Float32Array([
+    1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  // v0-v1-v2-v3 front(blue)
+    1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  // v0-v3-v4-v5 right(green)
+    1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  // v0-v5-v6-v1 up(red)
+    1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  // v1-v6-v7-v2 left
+    1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  // v7-v4-v3-v2 down
+    1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0   // v4-v7-v6-v5 back
+  ])
+  const normals = new Float32Array([
+    0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,   0.0, 0.0, 1.0,  // v0-v1-v2-v3 front
+    1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,   1.0, 0.0, 0.0,  // v0-v3-v4-v5 right
+    0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,   0.0, 1.0, 0.0,  // v0-v5-v6-v1 up
+   -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  -1.0, 0.0, 0.0,  // v1-v6-v7-v2 left
+    0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,   0.0,-1.0, 0.0,  // v7-v4-v3-v2 down
+    0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0,   0.0, 0.0,-1.0   // v4-v7-v6-v5 back
+  ])
+
   const indices = new Uint8Array([ // 无符号八位整型数
     0, 1, 2,   0, 2, 3,    // front
-    0, 3, 4,   0, 4, 5,    // right
-    0, 5, 6,   0, 6, 1,    // up
-    1, 6, 7,   1, 7, 2,    // left
-    7, 4, 3,   7, 3, 2,    // down
-    4, 7, 6,   4, 6, 5     // back
+    4, 5, 6,   4, 6, 7,    // right
+    8, 9,10,   8,10,11,    // up
+    12,13,14,  12,14,15,    // left
+    16,17,18,  16,18,19,    // down
+    20,21,22,  20,22,23     // back
   ])
-  const n = indices.length;
 
-  /**
-   * 1. 为attribute分配存储空间
-   */
-  const FSIZE = verticesColors.BYTES_PER_ELEMENT;
-  const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  const a_Color = gl.getAttribLocation(gl.program, 'a_Color');
-  
-  /** 
-   * 1. 创建缓冲区
-   * 2. 绑定缓冲区
-   * 3. 绑定数据
-   * 4. 配置指针
-   * 5. 开启缓冲区
-  */
-  const verticesColorBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, verticesColorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
-  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, FSIZE * 6, 0);
-  gl.enableVertexAttribArray(a_Position);
+
+  initArrayBuffer(gl, vertices, 3, gl.FLOAT, 'a_Position');
+  initArrayBuffer(gl, colors, 3, gl.FLOAT, 'a_Color');
+  initArrayBuffer(gl, normals, 3, gl.FLOAT, 'a_Normal');
 
   const indexBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
   gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
-  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 3);
-  gl.enableVertexAttribArray(a_Color);
 
-  return n
+  return indices.length
+
+}
+
+function initArrayBuffer(gl, data, num, type, attribute) {
+  const buffer = gl.createBuffer();   // Create a buffer object
+  if (!buffer) {
+    console.log('Failed to create the buffer object');
+    return false;
+  }
+  // 绑定缓冲区并将数据写入缓冲区
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+  // 分配各属性地址
+  const a_attribute = gl.getAttribLocation(gl.program, attribute);
+  if (a_attribute < 0) {
+    console.log('Failed to get the storage location of ' + attribute);
+    return false;
+  }
+  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+  // 启用缓冲对象的分配
+  gl.enableVertexAttribArray(a_attribute);
+
+  return true;
 }
